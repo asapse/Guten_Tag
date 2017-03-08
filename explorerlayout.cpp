7b2e78f7e64d2df3c83cba4957e299f9e5959804
Kevin_Espasa <kevin.espasa@etu.univ-nantes.fr>
2017-03-08 23:22:01 +0100

    ajout liste et barre de recherche

--- explorerlayout.cpp
+++ explorerlayout.cpp
@@ -1,23 +1,42 @@
 #include "explorerlayout.hpp"
 
-#include <QLineEdit>
-#include <QVBoxLayout>
-
 explorerlayout::explorerlayout(): QWidget()
 {
-    this->setStyleSheet("background-color:grey;");
+    _vlayout = new QVBoxLayout;
 
-    _qfilemodel = new QFileSystemModel();
-    _qtableview = new QTableView();
+    _qfilemodel = new QFileSystemModel(this);
 
-    QVBoxLayout *vlayout = new QVBoxLayout;
+    _qtableview = new QTableView(this);
+    _qtableview->setModel(_qfilemodel);
+    _qtableview->setRootIndex(_qfilemodel->setRootPath("D:/Documents/"));
+    _path = new QLineEdit();
+    _path->setStyleSheet("background-color:white;");
+    _path->setText(_qfilemodel->rootPath());
 
-    //add here new widgets, then add them to the layout
-    QLineEdit *recherche = new QLineEdit("/chemin/vers/dossier");
-    recherche->setStyleSheet("background-color:white;");
-    recherche->setReadOnly(true);
+    _vlayout->addWidget(_path);
+    _vlayout->addWidget(_qtableview);
 
-    vlayout->addWidget(recherche);
+    this->setLayout(_vlayout);
+    connect(_qtableview, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(on_qtableview_doubleClicked(QModelIndex)));
+    connect(_path, SIGNAL(returnPressed()), this, SLOT(on_path_returnPressed()));
 
-    this->setLayout(vlayout);
+
+}
+
+void explorerlayout::on_qtableview_doubleClicked(const QModelIndex &index)
+{
+        if(index.column() == 0){
+            _qtableview->setRootIndex(_qfilemodel->setRootPath("D:/Documents/"+index.data().toString()));
+            _path->setText(_qfilemodel->rootPath());
+        }
+}
+
+void explorerlayout::on_path_returnPressed()
+{
+    if(QDir(_path->text()).exists())
+        _qtableview->setRootIndex(_qfilemodel->setRootPath(_path->text()));
+    else
+        QMessageBox::warning(this,"Erreur à l'ouverture du chemin","Le chemin n'existe pas. Vérifiez que le nom est le bon.");
+
+
 }
